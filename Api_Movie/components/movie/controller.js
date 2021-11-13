cae4d6bf36c098364d71db9369237e4f849fd7ad
Manuel Mendoza <mj.mendoza.trabajo@gmail.com>
2021-11-13 10:43:25 +0100

    modificated User, Auth, and Movie

--- Api_Movie/components/movie/controller.js
+++ Api_Movie/components/movie/controller.js
@@ -1,5 +1,3 @@
-const { query } = require('express');//esto como no lo usamos podriamos quitarlo 
-const { model } = require('mongoose');//esto tambien no se usa
 const Movie = require('./model');
 
 // Método para crear una película en la BBDD.
@@ -23,32 +21,6 @@ module.exports.createMovie = async (req, res) => {
     }
 };
 
-// Método para buscar todas las películas.
-
-module.exports.getMovieCollection = async (req, res) => {
-    try {
-        const movie = await Movie.findById(req.params.id)
-        if (movie) {
-            res.json(movie);
-        } else {
-            res.json({
-                message: "movie not found"
-            }, 404);
-        }
-    } catch (error) {
-        console.error(error);
-        res.json({
-            message: error.message
-        }, 500);
-    }
-    /**
-     * Guille lo usa por find pero creo que es mejor por id no se como veais 
-     * const movie = await Movie.find({});
-     * res.json(movie);
-     */
-    
-    
-};
 
 // Método para buscar a través de uno de los valores del documento Movies.
 
@@ -81,18 +53,6 @@ module.exports.getMovieByKey = async (req, res) => {
             message: error.message
         }, 500);
     }
-
-/**
- * 
- * const query = {};
- *  if(req.query.title)query.title = req.query.title;
- *  if(req.query.director)query.director = req.query.director;
- *  if(req.query.genre)query.genre = req.query.genre;
- *  if(req.query.year)query.year = req.query.year;
- * const movie = await Movie.find(query);
- * res.json(movie);
- */
-    
 };
 
 // Método para buscar película por ID.
@@ -114,12 +74,6 @@ module.exports.getMovieById = async (req, res) => {
             message: error.message
         }, 500);
     }
-
-/**
- * 
- * const movie = await Movie.findById({_id: req.params.id})
- * res.json(movie)
- */
 };
 
 // Método para borrar películas. a través del ID.
@@ -127,7 +81,7 @@ module.exports.getMovieById = async (req, res) => {
 module.exports.deleteMovie = async (req, res) => {
     
     try {
-        const movie = await Movie.findById(req.params.id)
+        const movie = await Movie.findById(req.params.id);
         if (movie) {
             const movie = await Movie.findByIdAndDelete(req.params.id);
             res.json({
@@ -143,13 +97,6 @@ module.exports.deleteMovie = async (req, res) => {
             message: error.message
         }, 500);
     }
-
-
-    /**
-     * Guille lo hizo asi, aqui no contempla errores
-     * const movie = await Movie.findByIdAndDelete({_id: req.params.id})
-     * res.json({movie : Movie})
-    */
 };
 
 // Método para modificar un valor de películas a través de una búsqueda por ID.
@@ -178,10 +125,4 @@ module.exports.modifyMovie = async (req, res) => {
             }, 500);
         }
     }
-
-/**
- * aqui no le devuelve el valo de movie y tampoco contempla que exista o no una pelicula 
- * const movie = await Movie.findByIdAndUpdate({_id: req.params.id}, req.body)
- * res.json({movie : Movie})
-*/
 };
