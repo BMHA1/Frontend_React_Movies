3764647b4f38861f17efd4c7f843d1a348bcb712
BMHA1 <86425408+BMHA1@users.noreply.github.com>
2021-11-15 21:39:40 +0100

    metodo creaRental terminado

--- Api_Movie/components/rentals/controller.js
+++ Api_Movie/components/rentals/controller.js
@@ -1,88 +1,50 @@
 const Rental = require('./model');
 const Movie = require('../movie/model');
 const moment = require('moment');
-
 // Método para crear un pedido
 
+console.log(typeof moment())
+
 module.exports.createRental = async (req, res) => {
-    const userId = req.token._id ;
-    const rentalData = req.body; //peliculasId
-    const rentalDate = moment() ;
-    const expirationDate = rentalDate.clone().add(8, "days");
-    const precioSuma = 0 ;
-//recorro array rentaldata de ahi cojo el precio de cada una peliculas buscadas hacemos un reduce del precio de cada pelicula
 
-    try {
-
-        for (let i = 0; i < rentalData.length; i++) {
-            const element = rentalData[i];
-            const cadaPelicula = await Movie.findById(element);
-            for (let o = 0; o < rentalData.length; o++) {
-                const busquedaIgual = rentalDatal[o];
-                if (busquedaIgual == element) {
-                    res.status(404).json({message:'a movie is duplicate'})
-                }
-                
-            }
-            precioSuma += cadaPelicula.precio
+    const arrayMovie = req.body.movieId
+    const arrayPrice = await Promise.all(arrayMovie.map(async (value) => {
+        try {
+            const objectResult = await Movie.findById({ _id: value });
+            return objectResult.price
+        } catch (e) {
+            console.log(e)
         }
-        const newRental = new Rental({
-            userId: userId,
-            moviesID: rentalData,
-            totalPrice: precioSuma, 
-            rentalDate: rentalDate,
-            expirationDate: expirationDate
-        });
-
-        await newRental.save();
-
+    })
+    )
+    let result = arrayPrice.reduce((a, b) => a + b)
+    try {
+        console.log(typeof req.body.userId)
+        const newRental = new Rental(req.body);
+        newRental.totalPrice = result
+        newRental.rentalDate = moment()
+        newRental.expirationDate = newRental.rentalDate.clone().add(8, "days"),
+            await newRental.save();
+        res.status(404).json({ message: 'is good' })
     } catch (error) {
-        
     }
 };
-// module.exports.createRental = async (req, res) => {
-//     console.log(req.body)
-//     try {
-//         const orderCreate = new Rental(req.body);
-//         console.log(orderCreate)
-//         // orderCreate = { 
-//         //     "idUsuario": (populate,
-//         //     "idMovies": (populate)[{
-//         //         "name": "title",
-//         //          "price": 2
-//         //     }],
-//         //     "totalPrice": {totalprice:function cambiar(e)},
-//         //     "rentalDate": Date.now(),
-//         //     "expirateDate": Date.now().add(7, "days")    }
-//         await Rental.save();
-//         res.json({ movie:"hola mundo"});
-
-//     } catch (e) {
-//         console.log(e)
-//         res.status(400).send(`dont create order ${e}`)
-//     }
-// }
-
-// Método para ver todos sus pedidos por ID ??  pedidos (solo admin and user )
-
 module.exports.getRentals = async (req, res) => {
-    let filter = {} ;
-    
+    let filter = {};
+
     if (req.token.role == 'USER') {
-        filter.userId = req.token._id        
+        filter.userId = req.token._id
     }
 
     try {
         const rentals = await Rental.find(filter);
         res.json(rentals);
-        
+
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 };
-
 // // Método para buscar rental id 
-
 module.exports.getRentalId = async (req, res) => {
     try {
         const rental = await Rental.findById(req.query.id);
@@ -100,16 +62,3 @@ module.exports.getRentalId = async (req, res) => {
     }
 };
 
-// // Método para borrar películas. a través del ID.
-
-// module.exports.deleteMovie = async (req, res) => {
-//     const movie = await Movie.findByIdAndDelete({ _id: req.params.id })
-//     res.json({ movie: Movie })
-// }
-
-// // Método para modificar un valor de películas a través de una búsqueda por ID.
-
-// module.exports.modifyMovie = async (req, res) => {
-//     const movie = await Movie.findByIdAndUpdate({ _id: req.params.id }, req.body)
-//     res.json({ movie: Movie })
-// }
--- Api_Movie/components/rentals/helper/totalprice.js
+++ Api_Movie/components/rentals/helper/totalprice.js
@@ -1,10 +1,17 @@
+const Movie = require('../../movie/model');
 
 
+module.exports.totalprice= async(...price)=>{
 
-
-module.exports.totalprice=(...price)=>{
-
-
-
+    const res = await Movie.findOne().map(res => {
+        // Sets a `loadedAt` property on the doc that tells you the time the
+        // document was loaded.
+        return res == null ?
+          res :
+          Object.assign(res, { loadedAt: new Date() });
+      });
+    
 
 }
+
+   
