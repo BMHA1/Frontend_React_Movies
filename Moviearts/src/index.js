f8734872c7286af91f57c6ffd79c7536e1ee1233
BMHA1 <86425408+BMHA1@users.noreply.github.com>
2021-11-17 08:59:07 +0100

    Login y singUp estructurado, falta logica y css

--- Moviearts/src/index.js
+++ Moviearts/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Auth0Provider } from 'Auth0/auth0-react'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0Provider domain="moviearts.eu.auth0.com"  
-    clienteid="3l2EmKt7reM0KwWych9erljUVIHop4Ok"
-     redirectUri={window.Location.origin}>
+
+    
       <App />
-    </Auth0Provider>
+  
 
   </React.StrictMode>,
   document.getElementById('root')
